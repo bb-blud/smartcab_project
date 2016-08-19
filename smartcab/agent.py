a3f31577c1097cbfc5633b43252b09f00ed16a74
bb-blud <rsalo001@gmail.com>
2016-08-19 18:55:44 -0400

    Moved experimental "semi_reckless" action selection to a separate function. Added a dictionary to tally the number of bad_actions the agent takes after choosing an action to take. So far a bad_action is one that yeilds a negative reward. Also added a stats method to display a histograph of the number of bad moves the agent makes in a given trial. Moved a couple of lines in simulator.py to make it easier for me to read.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -3,18 +3,22 @@ from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 
+number_trials = 100
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
     def __init__(self, env):
-        super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
-        self.color = 'red'  # override color
+        super(LearningAgent, self).__init__(env)     # sets self.env = env, state = None, next_waypoint = None, and a default color
+        self.color = 'red'                           # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
 
         # TODO: Initialize any additional variables here
-        self.actions = [None, 'forward', 'left', 'right']
+        self.actions = self.env.valid_actions
+        self.bad_actions = {}
+        self.trial = -1 
         self.lights  = ['green','red']
         self.gamma = 0.5
+        
         self.Q = {
 
                   (action, light, waypoint, oncoming, left, right) : 0   \
@@ -25,7 +29,6 @@ class LearningAgent(Agent):
                   for left     in self.actions                           \
                   for right    in self.actions                           \
         }
-#        print "\n\n\n", self.Q[(None, 'green', 'left', None, None, None)], "\n\n"
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -38,7 +41,7 @@ class LearningAgent(Agent):
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
         wp = self.next_waypoint
-
+        
         # # Update state
         state = (inputs['light'], wp, inputs['oncoming'], inputs['left'], inputs['right'])
         
@@ -46,21 +49,52 @@ class LearningAgent(Agent):
         current_actions = {action : self.Q[ (action,) + state ] for action in self.actions }
         Q_action = max( current_actions, key=current_actions.get ) # This is the action that has the highest value in Q at the present state 
 
-        Q_value  = self.Q[ (Q_action,) + state ]   ## This is how Q rates the above Q_action (the actual max)
-        wp_value = self.Q[ (wp,) + state ]         ## This is how Q rates the way_point
-        
-        # Making the best choice given our current state
-        urgency = 1./(deadline + 0.001)
-        two_choices = {Q_action : Q_value * (1- urgency), wp : wp_value * urgency }
-        action = max(two_choices, key=two_choices.get)
+        action = wp   #reckless   
+        #action = self.semi_reckless(Q_action, state)
+        #action = Q_action
 
-        # Execute action and get reward
+        # # Execute action and get reward
         reward = self.env.act(self, action)
 
-        # TODO: Learn policy based on state, action, reward
+        # # Learn policy based on state, action, reward
         self.Q[ (action,) + state ] += reward * self.gamma
+        
+        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        # # Tally bad actions
+        location = self.env.agent_states[self]['location']
+        destination = self.env.agent_states[self]['destination']
+        dist = self.env.compute_dist(location, destination)
+        
+        if t == 0:
+            self.trial += 1
+            self.bad_actions[self.trial] = 0
+
+        if reward < 0:
+            self.bad_actions[self.trial] += 1
+            
+        if self.trial == number_trials - 1 and (deadline < 1  or dist < 1):
+            self.stats()
+        # if deadline < 1 or dist < 1:
+        #     print "AAAAAH!!! Trial {} BA = {}".format(self.trial, self.bad_actions[self.trial])
+        
+    def semi_reckless(self, Q_action, state):
+        wp = self.next_waypoint
+        deadline = self.env.get_deadline(self)
+        Q_value  = self.Q[ (Q_action,) + state ]   ## This is how Q rates the above Q_action (the actual max)
+        wp_value = self.Q[ (wp,) + state ]         ## This is how Q rates the way_point
+
+        # Making the best choice given our current state
+        urgency = 1./(deadline + 0.001)
+        two_choices = {Q_action : Q_value * (1 - urgency), wp : wp_value * urgency }
+
+        return max(two_choices, key=two_choices.get)
+        
+    def stats(self):
+        import matplotlib.pyplot as plt
+        plt.bar(range(len(self.bad_actions)), self.bad_actions.values(), align='center')
+        plt.xticks(range(len(self.bad_actions)), self.bad_actions.keys())
+        plt.show()
 
 
 def run():
@@ -73,10 +107,10 @@ def run():
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.0, display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
-    sim.run(n_trials=100)  # run for a specified number of trials
+    sim.run(n_trials=number_trials)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
 
--- smartcab/simulator.py
+++ smartcab/simulator.py
@@ -47,7 +47,11 @@ class Simulator(object):
                 self.agent_sprite_size = (32, 32)
                 self.agent_circle_radius = 10  # radius of circle, when using simple representation
                 for agent in self.env.agent_states:
-                    agent._sprite = self.pygame.transform.smoothscale(self.pygame.image.load(os.path.join("images", "car-{}.png".format(agent.color))), self.agent_sprite_size)
+                    agent._sprite = self.pygame.transform.smoothscale(
+                        self.pygame.image.load(
+                        os.path.join( 
+                        "images", "car-{}.png".format(agent.color))), self.agent_sprite_size)
+
                     agent._sprite_size = (agent._sprite.get_width(), agent._sprite.get_height())
 
                 self.font = self.pygame.font.Font(None, 28)
@@ -112,14 +116,21 @@ class Simulator(object):
         # Draw elements
         # * Static elements
         for road in self.env.roads:
-            self.pygame.draw.line(self.screen, self.road_color, (road[0][0] * self.env.block_size, road[0][1] * self.env.block_size), (road[1][0] * self.env.block_size, road[1][1] * self.env.block_size), self.road_width)
+            self.pygame.draw.line(self.screen, self.road_color, 
+                                  (road[0][0] * self.env.block_size, road[0][1] * self.env.block_size), 
+                                  (road[1][0] * self.env.block_size, road[1][1] * self.env.block_size), 
+                                  self.road_width)
 
         for intersection, traffic_light in self.env.intersections.iteritems():
-            self.pygame.draw.circle(self.screen, self.road_color, (intersection[0] * self.env.block_size, intersection[1] * self.env.block_size), 10)
+            self.pygame.draw.circle(self.screen, self.road_color, 
+                                    (intersection[0] * self.env.block_size, intersection[1] * self.env.block_size), 
+                                    10)
+
             if traffic_light.state:  # North-South is open
                 self.pygame.draw.line(self.screen, self.colors['green'],
                     (intersection[0] * self.env.block_size, intersection[1] * self.env.block_size - 15),
                     (intersection[0] * self.env.block_size, intersection[1] * self.env.block_size + 15), self.road_width)
+
             else:  # East-West is open
                 self.pygame.draw.line(self.screen, self.colors['green'],
                     (intersection[0] * self.env.block_size - 15, intersection[1] * self.env.block_size),
@@ -131,9 +142,13 @@ class Simulator(object):
             agent_offset = (2 * state['heading'][0] * self.agent_circle_radius, 2 * state['heading'][1] * self.agent_circle_radius)
             agent_pos = (state['location'][0] * self.env.block_size - agent_offset[0], state['location'][1] * self.env.block_size - agent_offset[1])
             agent_color = self.colors[agent.color]
+
             if hasattr(agent, '_sprite') and agent._sprite is not None:
                 # Draw agent sprite (image), properly rotated
-                rotated_sprite = agent._sprite if state['heading'] == (1, 0) else self.pygame.transform.rotate(agent._sprite, 180 if state['heading'][0] == -1 else state['heading'][1] * -90)
+                rotated_sprite = agent._sprite if state['heading'] == (1, 0)                   \
+            else self.pygame.transform.rotate(agent._sprite, 180 if state['heading'][0] == -1  \
+            else state['heading'][1] * -90)                                                    \
+
                 self.screen.blit(rotated_sprite,
                     self.pygame.rect.Rect(agent_pos[0] - agent._sprite_size[0] / 2, agent_pos[1] - agent._sprite_size[1] / 2,
                         agent._sprite_size[0], agent._sprite_size[1]))
@@ -141,12 +156,20 @@ class Simulator(object):
                 # Draw simple agent (circle with a short line segment poking out to indicate heading)
                 self.pygame.draw.circle(self.screen, agent_color, agent_pos, self.agent_circle_radius)
                 self.pygame.draw.line(self.screen, agent_color, agent_pos, state['location'], self.road_width)
-            if agent.get_next_waypoint() is not None:
-                self.screen.blit(self.font.render(agent.get_next_waypoint(), True, agent_color, self.bg_color), (agent_pos[0] + 10, agent_pos[1] + 10))
-            if state['destination'] is not None:
-                self.pygame.draw.circle(self.screen, agent_color, (state['destination'][0] * self.env.block_size, state['destination'][1] * self.env.block_size), 6)
-                self.pygame.draw.circle(self.screen, agent_color, (state['destination'][0] * self.env.block_size, state['destination'][1] * self.env.block_size), 15, 2)
 
+            if agent.get_next_waypoint() is not None:
+                self.screen.blit(self.font.render(agent.get_next_waypoint(), 
+                                                  True, agent_color, self.bg_color), (agent_pos[0] + 10, agent_pos[1] + 10))
+            if state['destination'] is not None:
+                self.pygame.draw.circle(self.screen, 
+                                        agent_color, 
+                                        (state['destination'][0] * self.env.block_size, state['destination'][1] * self.env.block_size), 
+                                        6)
+
+                self.pygame.draw.circle(self.screen, 
+                                        agent_color, 
+                                        (state['destination'][0] * self.env.block_size, state['destination'][1] * self.env.block_size), 
+                                        15, 2)
         # * Overlays
         text_y = 10
         for text in self.env.status_text.split('\n'):
